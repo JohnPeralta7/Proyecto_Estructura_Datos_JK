7026d17dbc2afb557c1a016deb8786137f158c72
JohnPeralta7 <jperaltar3@unemi.edu.ec>
2024-10-24 21:56:28 -0500

    Añadido instrucciones extra y una funcion extra de la fecha

--- data.py
+++ data.py
@@ -1,14 +1,22 @@
+from datetime import datetime
+now = datetime.now()
 def num_words(args): #funcion que da cantidad de palabras
     words = args.split()
     quantity = len(words)
     return quantity
+
+def t_day():
+    today = str(now.date())
+    x = today.split('-')
+    return x
+
 
 
 class Homework: #Clase Padre - se usará más adelante para el gestor
     def __init__(self):
         self.name = ''
         self.description = ''
-        self.priority = ''
+        self.date = ''
 
     def get_all(self): #Me muestra toda la info de la tarea y valida si hay algo que mostrar o no
         if self.name != '' and self.description != '' and self.priority != '':
@@ -34,21 +42,26 @@ class Homework: #Clase Padre - se usará más adelante para el gestor
         else:
             print('Límite es de 14 palabras!!')
 
-    def set_priority(self, priority):#Valida la prioridad
-        if priority.lower() == 'alta':
-            self.priority = priority.upper()
-            return self.priority
-        elif priority.lower() == 'media':
-            self.priority = priority.upper()
-            return self.priority
-        elif priority.lower() == 'baja':
-            self.priority = priority.upper()
-            return self.priority
+    def set_date(self, year, month, day):#Pide fecha al usuario y la compara con la fecha actual
+        date = []
+        if str(type(year)) != "<class 'str'>":
+            date.append(year)
         else:
-            print('Valor no Válido')
+            print('Ingresa el año como valor númerico')
+        if str(type(month)) != "<class 'str'>":
+            date.append(month)
+        else:
+            print('Ingresa el mes como valor númerico')
+        if str(type(day)) != "<class 'str'>":
+            date.append(day)
+        else:
+            print('Ingresa el dia como valor númerico')
+
+        today = t_day() #una funcion que me retorna un array con valores string de la fecha actual
+        pass
 
 class M_Homework(Homework):#Clase de gestion de tarea - hereda la clase homework
-    def save(self, priority, titule = input('Nombre de archivo : ')): # guardar(crear) archivo con tarea inicial
+    def save(self, titule): # guardar(crear) archivo con tarea inicial
         titule += '.txt'
         arch = open(f'file/{titule}', 'w')
         arch.write(f'[☺︎ {self.set_name(name = input('Ingresa nombre de tarea : '))} (PENDIENTE)')
@@ -57,7 +70,7 @@ class M_Homework(Homework):#Clase de gestion de tarea - hereda la clase homework
         arch.write(f'\n     ({self.set_description(description = input('Ingrese descripcion'))})')
         arch.close()
         arch = open(f'file/{titule}', 'a')
-        arch.write(f'\n      PRIORIDAD {self.set_priority(priority)}]\n----------------------------------------')
+        arch.write(f'\n      PRIORIDAD {'correcion'}]\n----------------------------------------')
         arch.close()
 
 
@@ -65,12 +78,5 @@ class M_Homework(Homework):#Clase de gestion de tarea - hereda la clase homework
 #y segun los dias que falten por hacerla poner su prioridad, tipo si es en 3 dias que hay que terminar la tarea que sea ALTA
 #entonces dependiendo eso, reestructurar save en la parte prioridad
 #modificar set priority en la clase padre, para que pida fecha y compare con la actual para luego tomarlo con validar prioridad y usarlo en save, pilas!!
-x = M_Homework()
 
-
-
-
-
-
-
-
+print(t_day())
